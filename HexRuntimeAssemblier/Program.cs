ff963af089cb3640d2b80bfd7e66b75e0c56e128
HexJacaranda <rbqlzg1937n@msn.com>
2021-08-18 15:54:03 +0800

    Add project files.

---
+++ HexRuntimeAssemblier/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HexRuntimeAssemblier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
